0ee34b2b6593d13e054f95726ab6ae789e87eb58
Marcin Kulik <m@ku1ik.com>
2014-07-30 13:19:26 +0200

    Add basic logging

--- archive_cache.go
+++ archive_cache.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"os"
 )
 
@@ -20,15 +21,18 @@ func ArchiveCache(
 			case job := <-jobs:
 				cachedPath := fmt.Sprintf("%v/%v", cachePath, job.Filename)
 				if _, err := os.Stat(cachedPath); err == nil {
+					log.Println("cache: hit for %v", job)
 					job.Result = &ArchiveResult{cachedPath, nil}
 					results <- job
 				} else {
+					log.Println("cache: miss for %v", job)
 					upstreamJobs <- job
 				}
 
 			case job := <-upstreamResults:
 				result := job.Result
 				if result.Error == nil {
+					log.Println("cache: saving result for %v", job)
 					cachedPath := fmt.Sprintf("%v/%v", cachePath, job.Filename)
 					os.Rename(result.Path, cachedPath)
 					job.Result = &ArchiveResult{cachedPath, nil}
--- request_mux.go
+++ request_mux.go
@@ -1,5 +1,7 @@
 package main
 
+import "log"
+
 func RequestMux(jobs chan *ArchiveJob, results chan *ArchiveJob) chan *ArchiveRequest {
 	requests := make(chan *ArchiveRequest)
 	queues := make(map[string][]*ArchiveRequest)
@@ -9,14 +11,17 @@ func RequestMux(jobs chan *ArchiveJob, results chan *ArchiveJob) chan *ArchiveRe
 			select {
 			case request := <-requests:
 				job := request.Job
+				log.Printf("mux: appending request %v to queue for %v", request, job.Filename)
 				queues[job.Filename] = append(queues[job.Filename], request)
 
 				if len(queues[job.Filename]) == 1 {
+					log.Printf("mux: scheduling job %v", job)
 					go func() {
 						jobs <- job
 					}()
 				}
 			case job := <-results:
+				log.Printf("mux: sending job result to requests from queue for %v", job.Filename)
 				for _, request := range queues[job.Filename] {
 					request.ResultChan <- job.Result
 				}
